46a58942704822b7a70d78195068585fa2cfe977
c0a24017 <c0a24017@edu.teu.ac.jp>
2025-04-15 16:12:59 +0900

    練習問題9:残像解消

--- pygame_image.py
+++ pygame_image.py
@@ -9,17 +9,20 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん") #2
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img,True,False)
+    bg_img2 = pg.transform.flip(bg_img,True,False) #8
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True ,False)
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr
+        x=tmr%3200 #9
         screen.blit(bg_img, [-x, 0])   #6
         screen.blit(bg_img2,[1600-x,0]) #7 もう一枚追加  
+        screen.blit(bg_img,[3200-x,0]) #9 もう一枚追加 
         screen.blit(kk_img, [300, 200]) #4
         pg.display.update()
         tmr += 1        
