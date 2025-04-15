73fcc01fb53031c0f7f487e46b6868d45e48684d
c0a24017 <c0a24017@edu.teu.ac.jp>
2025-04-15 17:19:39 +0900

    演習課題:2

--- pygame_image.py
+++ pygame_image.py
@@ -15,26 +15,29 @@ def main():
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True ,False)
     kk_rct= kk_img.get_rect() #10-1
-    kk_rct.center=[300,200]  #10-2
+    kk_rct.center=300,200  #10-2
     tmr = 0
 
     while True:
+        X=-1
+        Y=0
+
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
         key_lst=pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1)) #10-4
+            Y-=1 #10-4
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1)) #10-4
+            Y+=1 #10-4
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0)) #10-4
+            X+=2 #10-4
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-2,0)) #10-4
-
+            X-=1 #10-4
         x=tmr%3200 #9
-        y=kk_rct.center[0]-1
-        kk_rct.center=[y,200]
-        
+
+        kk_rct.move_ip(X,Y)
+
         screen.blit(bg_img, [-x, 0])   #6
         screen.blit(bg_img2,[1600-x,0]) #7 もう一枚追加  
         screen.blit(bg_img,[3200-x,0]) #9 もう一枚追加
